0b89c4e2adb224f2d34173d78da27c1e6bb9a87a
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-10-16 20:20:09 +0530

    aading backend as express..

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express"
+import cors from "cors"
+import 'dotenv/config';
+
+
+const app = express()
+const port= process.env.PORT || 4000
+
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/",(req,res)=> {
+    res.send("its works")
+})
+
+
+app.listen(port,()=>{
+    console.log("server runs on ", port);
+    
+})
