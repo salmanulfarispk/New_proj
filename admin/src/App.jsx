09401e2bec164b2cbc1c3e5e38beb21ceb9d5e46
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-10-18 19:48:58 +0530

    adding admin login page..

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import { Route,Routes } from "react-router-dom"
@@ -6,32 +6,44 @@ import Home from "./pages/Home"
 import Add from "./pages/Add"
 import List from "./pages/List"
 import Orders from "./pages/Orders"
+import Login from './pages/Login'
 
 
 
 
 const App = () => {
+
+
+  const [token,setToken]=useState("")
+
   return (
     <div className='bg-gray-50 min-h-screen'>
+     
+     { token === "" ? (
+       <Login />
+     ):(
+
       <>
-     <Navbar />
-      <hr/>
-      <div className='flex w-full'>
-        <Sidebar/>
-        <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+      <Navbar />
+       <hr/>
+       <div className='flex w-full'>
+         <Sidebar/>
+         <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+ 
+            <Routes>
+ 
+             <Route path='/' element={<Home/>}/>
+             <Route path='/add-product' element={<Add/>}/>
+             <Route path='/list-product' element={<List/>}/>
+             <Route path='/order-details' element={<Orders/>}/>
+ 
+            </Routes>
+ 
+         </div>
+       </div>
+       </>
 
-           <Routes>
-
-            <Route path='/' element={<Home/>}/>
-            <Route path='/add-product' element={<Add/>}/>
-            <Route path='/list-product' element={<List/>}/>
-            <Route path='/order-details' element={<Orders/>}/>
-            
-           </Routes>
-
-        </div>
-      </div>
-      </>
+     )}
 
 
     </div>
--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='min-h-screen flex items-center justify-center w-full'>
+        <div className='bg-white shadow-md  rounded-lg px-8 py-6 max-w-md'>
+            <h1 className='text-2xl font-bold mb-4'>Admin Panel</h1>
+            <form>
+                <div className='mb-3 min-w-72'>
+                    <p>Email Address</p>
+                    <input type='email' placeholder='your@email.com' required
+                      className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none'
+                     />
+                </div>
+                <div className='mb-3 min-w-72'>
+                    <p>Password</p>
+                    <input type='password' placeholder='Enter your password' required
+                      className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none'
+                    />
+                </div>
+                <button type='submit' className='mt-2 w-full py-2 px-4 rounded-md text-white bg-black'>
+                    Login
+                </button>
+            </form>
+        </div>
+    </div>
   )
 }
 
